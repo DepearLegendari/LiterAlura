aff00fbfb5ceeb42d9675888be7cedc972f9af28
Iván E. S. Matías <evanvorn_1996@hotmail.com>
2026-02-01 16:04:58 -0600

    chore: inicializa proyecto LiterAlura con Spring Boot

---
+++ src/main/java/com/alura/BoockSearch/BoockSearchApplication.java
@@ -0,0 +1,13 @@
+package com.alura.BoockSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoockSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoockSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/BoockSearch/BoockSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.BoockSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoockSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
