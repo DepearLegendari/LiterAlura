2b11be505c78e22e246bdd72a966591bdbbc26ba
Iván E. S. Matías <evanvorn_1996@hotmail.com>
2026-02-01 16:54:47 -0600

    feat: consume API de libros Gutendex

--- src/main/java/com/alura/BoockSearch/BoockSearchApplication.java
+++ src/main/java/com/alura/BoockSearch/BoockSearchApplication.java
@@ -2,12 +2,20 @@ package com.alura.BoockSearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import service.ConsumoAPI;
 
 @SpringBootApplication
 public class BoockSearchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoockSearchApplication.class, args);
+
+        ConsumoAPI consumo = new ConsumoAPI();  // -> SOLO TRAE EL JSON, NÓ LO INTERPROTA
+        String json = consumo.obtenerDatos(
+                "https://gutendex.com/books/?search="
+        );
+
+        System.out.println(json);
 	}
 
 }
