4b438b4b0e556d934eb77c83a285f52725734f25
Iván E. S. Matías <evanvorn_1996@hotmail.com>
2026-02-01 22:49:17 -0600

    fix: ignora campos desconocidos en mapeo JSON

--- src/main/java/com/alura/BoockSearch/model/DatosLibro.java
+++ src/main/java/com/alura/BoockSearch/model/DatosLibro.java
@@ -1,11 +1,16 @@
 package com.alura.BoockSearch.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosLibro(
         Integer id,
         String title,
         List<DatosAutor> authors,
+        @JsonProperty("language")
         List<String> lenguages,
         Integer download_count
 
--- src/main/java/com/alura/BoockSearch/model/DatosRespuesta.java
+++ src/main/java/com/alura/BoockSearch/model/DatosRespuesta.java
@@ -1,7 +1,10 @@
 package com.alura.BoockSearch.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosRespuesta (
         Integer count,
         List<DatosLibro> results
